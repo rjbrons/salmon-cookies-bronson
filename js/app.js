9fe1ec10f67086cf2b02fbf7eda8ebc732837b2f
Rob Bronson <robert.j.bronson+cf@gmail.com>
2019-04-01 16:41:12 -0700

    adding elements to page

--- js/app.js
+++ js/app.js
@@ -23,23 +23,35 @@ let firstPike = {
   cookiePerSale: 6.3,
   cookiesPerHour: [],
   custPerHour: [],
-
+  //generate a random number of customers per hour based on location min/max
   generateCustomerLoad: function() {
     let custPossible = this.maxCust - this.minCust;
     return Math.floor(Math.random() * custPossible) + this.minCust;
   },
-
+  //generate cookies per hour based on customers per hour and hours of operation
   getCookiesPerHour: function() {
     this.hoursOfOperation.forEach(() => {
       this.cookiesPerHour.push(
-        this.generateCustomerLoad() * this.cookiesPerSale
+        Math.ceil(this.generateCustomerLoad() * this.cookiesPerSale)
       );
     });
   },
-
+  //calculate total daily cookies summing cookie sales for each hour
   calculateTotalCookies: function(perHourArray) {
     let total = 0;
     perHourArray.forEach(item => (total += item));
     return total;
   }
 };
+
+let ulEl = document.getElementById('firstPike');
+for (let i = 0; i < firstPike.hoursOfOperation.length; i++) {
+  let liEl = document.createElement('li');
+  liEl.innerText(
+    `${firstPike.hoursOfOperation[i]}: ${firstPike.cookiesPerHour[i]} cookies.`
+  );
+  ulEl.appendChild(liEl);
+}
+let liEl = document.createElement('li');
+liEl.innerText(`TOTAL: ${firstPike.calculateTotalCookies()} cookies`);
+ulEl.appendChild(liEl);
