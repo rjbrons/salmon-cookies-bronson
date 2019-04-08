952d47c650a25349c5e69fcccdd705536846f832
Rob Bronson <robert.j.bronson+cf@gmail.com>
2019-04-08 12:31:59 -0700

    fixed switching from home to sales pages and toggle of store locations

--- js/home.js
+++ js/home.js
@@ -1,11 +1,5 @@
 'use strict';
 
-console.log('JS change working');
-
-function doThis() {
-  console.log('This event fired.');
-}
-
 var storeWindow = document.getElementById('stores');
 
 function toggleStores(e) {
@@ -16,10 +10,6 @@ function toggleStores(e) {
     storeWindow.style.display = 'none';
   }
 }
-
-var testEl = document.getElementsByTagName('body')[0];
 var storeButton = document.getElementsByClassName('buttons')[2];
 
-testEl.addEventListener('click', toggleStores);
-// storeWindow.addEventListener('click', toggleStores);
 storeButton.addEventListener('click', toggleStores);
